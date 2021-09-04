b71b5dc3e027dba27c64056b55441e89cd11f8b0
RenanEvangelista <renan103326773911@gmail.com>
2021-09-04 04:34:32 -0300

    test: should not be able to create a user with the same email

--- tests/services/CreateUserService.spec.ts
+++ tests/services/CreateUserService.spec.ts
@@ -1,14 +1,19 @@
 import FakeUsersRepository from '@/repositories/fakes/FakeUsersRepository';
+import CreateUserService from '@/services/CreateUserService';
+
+import { EmailInUseError } from '@/errors';
 
 let fakeUsersRepository: FakeUsersRepository;
+let createUserService: CreateUserService;
 
 describe('CreateUserService', () => {
   beforeEach(() => {
     fakeUsersRepository = new FakeUsersRepository();
+    createUserService = new CreateUserService(fakeUsersRepository);
   });
 
   it('should be able to create user', async () => {
-    const user = await fakeUsersRepository.create({
+    const user = await createUserService.execute({
       name: 'test',
       email: 'test@test.com',
       password: 'test12345',
@@ -16,5 +21,23 @@ describe('CreateUserService', () => {
     });
 
     expect(user).toHaveProperty('id');
+  });
+
+  it('should not be able to create a user with the same email', async () => {
+    await fakeUsersRepository.create({
+      name: 'test',
+      email: 'test@test.com',
+      password: 'test12345',
+      telephones: [],
+    });
+
+    await expect(
+      createUserService.execute({
+        name: 'test2',
+        email: 'test@test.com',
+        password: 'test12345',
+        telephones: [],
+      }),
+    ).rejects.toBeInstanceOf(EmailInUseError);
   });
 });
