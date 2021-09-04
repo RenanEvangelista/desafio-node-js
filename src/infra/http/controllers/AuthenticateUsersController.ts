8d79bdf308fb0cea380318615972135b9a7fe4e4
RenanEvangelista <renan103326773911@gmail.com>
2021-09-04 06:11:46 -0300

    feat: add AuthenticateUserController

---
+++ src/infra/http/controllers/AuthenticateUsersController.ts
@@ -0,0 +1,57 @@
+import AuthenticateUserService from '@/services/AuthenticateUserService';
+import Validator from '@/validation/contracts/validator';
+import {
+  UserNotFoundError,
+  InvalidParamError,
+  MissingParamError,
+} from '@/errors';
+import { Controller, HttpResponse } from '../contracts';
+import {
+  success,
+  serverError,
+  unauthorized,
+  badRequest,
+} from '../helpers/http-helpers';
+
+class AuthenticateUsersController implements Controller {
+  constructor(
+    private readonly authenticateUserService: AuthenticateUserService,
+    private readonly validator: Validator,
+  ) {}
+
+  async handle(
+    request: AuthenticateUsersController.Request,
+  ): Promise<HttpResponse<string>> {
+    try {
+      this.validator.validate(request);
+      const token = await this.authenticateUserService.execute({
+        email: request.email,
+        password: request.password,
+      });
+      return success(token);
+    } catch (err) {
+      if (!(err instanceof Error)) {
+        return serverError(new Error());
+      }
+
+      switch (err.constructor) {
+        case UserNotFoundError:
+        case InvalidParamError:
+          return unauthorized(err);
+        case MissingParamError:
+          return badRequest(err);
+        default:
+          return serverError(err);
+      }
+    }
+  }
+}
+
+export namespace AuthenticateUsersController {
+  export type Request = {
+    email: string;
+    password: string;
+  };
+}
+
+export default AuthenticateUsersController;
