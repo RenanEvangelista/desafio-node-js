494f9be180969230e6191091e283460a8a56e620
RenanEvangelista <renan103326773911@gmail.com>
2021-09-04 16:13:19 -0300

    chore: config build commands

--- ormconfig.js
+++ ormconfig.js
@@ -5,8 +5,8 @@ module.exports = {
   username: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
-  entities: [`${__dirname}/dist/infra/db/typeorm/entities/*.{ts,js}`],
-  migrations: ['./dist/infra/db/typeorm/migrations/*.{ts,js}'],
+  entities: [`${__dirname}/{dist,src}/infra/db/typeorm/entities/*.{ts,js}`],
+  migrations: [`./{dist,src}/infra/db/typeorm/migrations/*.{ts,js}`],
   cli: {
     migrationsDir: './src/infra/db/typeorm/migrations',
   },
