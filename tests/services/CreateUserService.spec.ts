0d2be79846adce5082020bd44b82699cb9416b4a
RenanEvangelista <renan103326773911@gmail.com>
2021-09-04 04:42:46 -0300

    test: ensure the user's password is encrypted

--- tests/services/CreateUserService.spec.ts
+++ tests/services/CreateUserService.spec.ts
@@ -1,15 +1,21 @@
-import FakeUsersRepository from '@/repositories/fakes/FakeUsersRepository';
 import CreateUserService from '@/services/CreateUserService';
+import FakeUsersRepository from '@/repositories/fakes/FakeUsersRepository';
+import FakeHashProvider from '@/providers/HashProvider/fakes/FakeHashProvider';
 
 import { EmailInUseError } from '@/errors';
 
+let fakeHashProvider: FakeHashProvider;
 let fakeUsersRepository: FakeUsersRepository;
 let createUserService: CreateUserService;
 
 describe('CreateUserService', () => {
   beforeEach(() => {
+    fakeHashProvider = new FakeHashProvider();
     fakeUsersRepository = new FakeUsersRepository();
-    createUserService = new CreateUserService(fakeUsersRepository);
+    createUserService = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
   });
 
   it('should be able to create user', async () => {
@@ -39,5 +45,16 @@ describe('CreateUserService', () => {
         telephones: [],
       }),
     ).rejects.toBeInstanceOf(EmailInUseError);
+  });
+
+  it("ensure the user's password is encrypted", async () => {
+    const user = await createUserService.execute({
+      name: 'test',
+      email: 'test@test.com',
+      password: 'test12345',
+      telephones: [],
+    });
+
+    expect(user.password).not.toBe('test12345');
   });
 });
