d968943e7ca402fb8cf325d68b8d57e281c93f85
RenanEvangelista <renan103326773911@gmail.com>
2021-09-04 06:03:17 -0300

    feat: add http helper

---
+++ src/infra/http/helpers/http-helpers.ts
@@ -0,0 +1,23 @@
+import { HttpResponse } from '../contracts/HttpResponse';
+
+import { ServerError } from '../../../errors';
+
+export const success = (data: any): HttpResponse => ({
+  statusCode: 200,
+  data,
+});
+
+export const serverError = (error: Error): HttpResponse => ({
+  statusCode: 500,
+  data: new ServerError(error.stack || ''),
+});
+
+export const badRequest = (error: Error): HttpResponse => ({
+  statusCode: 400,
+  data: error,
+});
+
+export const unauthorized = (error: Error): HttpResponse => ({
+  statusCode: 401,
+  data: error,
+});
