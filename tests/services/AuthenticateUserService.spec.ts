cf34c4ce3b2e582f82646ebdb310c6fc3f8409e4
RenanEvangelista <renan103326773911@gmail.com>
2021-09-04 05:22:16 -0300

    test: should not be able to authenticate a user with an incorrect password

--- tests/services/AuthenticateUserService.spec.ts
+++ tests/services/AuthenticateUserService.spec.ts
@@ -1,7 +1,7 @@
 import AuthenticateUserService from '@/services/AuthenticateUserService';
 import FakeUsersRepository from '@/repositories/fakes/FakeUsersRepository';
 import FakeHashProvider from '@/providers/HashProvider/fakes/FakeHashProvider';
-import { UserNotFoundError } from '@/errors';
+import { InvalidParamError, UserNotFoundError } from '@/errors';
 
 let fakeHashProvider: FakeHashProvider;
 let fakeUsersRepository: FakeUsersRepository;
@@ -40,5 +40,21 @@ describe('AuthenticateUserService', () => {
         password: 'test12345',
       }),
     ).rejects.toBeInstanceOf(UserNotFoundError);
+  });
+
+  it('should not be able to authenticate a user with an incorrect password', async () => {
+    await fakeUsersRepository.create({
+      name: 'test',
+      email: 'test@test.com',
+      password: 'test12345',
+      telephones: [],
+    });
+
+    await expect(
+      authenticateUserService.execute({
+        email: 'test@test.com',
+        password: 'incorrect password',
+      }),
+    ).rejects.toBeInstanceOf(InvalidParamError);
   });
 });
