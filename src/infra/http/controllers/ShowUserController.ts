b12d352b624e8a44bef566c4fc91afeeca3d9244
RenanEvangelista <renan103326773911@gmail.com>
2021-09-04 06:08:33 -0300

    feat: add show user controller

---
+++ src/infra/http/controllers/ShowUserController.ts
@@ -0,0 +1,53 @@
+import ShowUserService from '../../../services/ShowUserService';
+import { Controller, HttpResponse } from '../contracts';
+import {
+  success,
+  serverError,
+  unauthorized,
+  badRequest,
+} from '../helpers/http-helpers';
+
+import {
+  UserNotFoundError,
+  InvalidParamError,
+  MissingParamError,
+} from '../../../errors';
+
+import { ShowUserViewModel } from '../view-models';
+
+class ShowUserController implements Controller {
+  constructor(private readonly showUserService: ShowUserService) {}
+
+  async handle(
+    request: ShowUserController.Request,
+  ): Promise<HttpResponse<ShowUserViewModel>> {
+    try {
+      const user = await this.showUserService.execute(request.user_id);
+      const viewModel = ShowUserViewModel.map(user);
+      return success(viewModel);
+    } catch (err) {
+      if (!(err instanceof Error)) {
+        return serverError(new Error());
+      }
+
+      switch (err.constructor) {
+        case UserNotFoundError:
+          return unauthorized(err);
+        case InvalidParamError:
+          return unauthorized(err);
+        case MissingParamError:
+          return badRequest(err);
+        default:
+          return serverError(err);
+      }
+    }
+  }
+}
+
+export namespace ShowUserController {
+  export type Request = {
+    user_id: string;
+  };
+}
+
+export default ShowUserController;
