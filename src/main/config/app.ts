d7a1ac02c3365897265d2fec5719955080802a7f
RenanEvangelista <renan103326773911@gmail.com>
2021-09-04 10:48:05 -0300

    feat: add cors

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 
 import { setupRoutes } from './routes';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 setupRoutes(app);
 
