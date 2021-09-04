feff3ea35643131943e327dc449441598cb38479
RenanEvangelista <renan103326773911@gmail.com>
2021-09-04 17:20:47 -0300

    fix: update ormconfig to prod

--- ormconfig.js
+++ ormconfig.js
@@ -3,14 +3,14 @@ module.exports = {
   url: process.env.DATABASE_URL,
   synchronize: true,
   logging: true,
-  entities: [`${__dirname}/{dist,src}/infra/db/typeorm/entities/*.{ts,js}`],
+  entities: [`${__dirname}/{dist}/infra/db/typeorm/entities/*.{ts,js}`],
   ssl: true,
   extra: {
     ssl: {
       rejectUnauthorized: false,
     },
   },
-  migrations: [`./{dist,src}/infra/db/typeorm/migrations/*.{ts,js}`],
+  migrations: [`./{dist}/infra/db/typeorm/migrations/*.{ts,js}`],
   cli: {
     migrationsDir: './src/infra/db/typeorm/migrations',
   },
