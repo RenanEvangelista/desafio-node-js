e0389fa58e602dd32b20319c6ef6f1b332d02146
RenanEvangelista <renan103326773911@gmail.com>
2021-09-04 17:15:15 -0300

    chore: update ormconfig to prod

--- ormconfig.js
+++ ormconfig.js
@@ -1,7 +1,15 @@
 module.exports = {
   type: 'postgres',
   url: process.env.DATABASE_URL,
+  synchronize: true,
+  logging: true,
   entities: [`${__dirname}/{dist,src}/infra/db/typeorm/entities/*.{ts,js}`],
+  ssl: true,
+  extra: {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
   migrations: [`./{dist,src}/infra/db/typeorm/migrations/*.{ts,js}`],
   cli: {
     migrationsDir: './src/infra/db/typeorm/migrations',
