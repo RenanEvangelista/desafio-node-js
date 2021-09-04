b84f17073014d0931f87e5d36540d3df87d8d11f
RenanEvangelista <renan103326773911@gmail.com>
2021-09-04 04:44:56 -0300

    feat: ensure the user's password is encrypted

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -4,9 +4,13 @@ import IUsersRepository from '../repositories/IUsersRepository';
 import { EmailInUseError } from '@/errors';
 
 import User from '../models/User';
+import IHashProvider from '@/providers/HashProvider/IHashProvider';
 
 class CreateUserService {
-  constructor(private readonly usersRepository: IUsersRepository) {}
+  constructor(
+    private readonly usersRepository: IUsersRepository,
+    private readonly hashProvider: IHashProvider,
+  ) {}
 
   async execute({
     email,
@@ -20,10 +24,12 @@ class CreateUserService {
       throw new EmailInUseError();
     }
 
+    const passwordHashed = await this.hashProvider.generate(password);
+
     const user = await this.usersRepository.create({
       email,
       name,
-      password,
+      password: passwordHashed,
       telephones,
     });
 
